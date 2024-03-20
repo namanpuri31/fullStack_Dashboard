7d7c6886d0a9b2a656182d91168b9c1ecbcc1f3c
Naman Puri <113339416+namanpuri31@users.noreply.github.com>
2024-03-20 17:30:22 +0530

    Update api.js

--- clientt/src/state/api.js
+++ clientt/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://backend-service-upub.onrender.com/" }),
     reducerPath: "adminApi",
     tagTypes: ["User", "Produts", "Customers", "Transactions", "Geography", "Sales", "Admins", "Performance", "Dashboard"],
     endpoints: (build) => ({
