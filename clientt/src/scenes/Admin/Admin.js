a57e4fbf9a6b8d0e07d55fe82f9a5ae0ec82d300
Naman Puri <113339416+namanpuri31@users.noreply.github.com>
2024-03-20 17:11:14 +0530

    Add files via upload
    committed client

---
+++ clientt/src/scenes/Admin/Admin.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { useGetAdminsQuery } from 'state/api'
+import { Box, useTheme } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import Header from 'components/Header'
+import { AddBoxRounded } from '@mui/icons-material'
+const Admin = () => {
+    const { data, isLoading } = useGetAdminsQuery()
+    const theme = useTheme()
+    const columns = [
+        {
+            field: "_id",
+            headerName: "ID",
+            flex: 1,
+        },
+        {
+            field: "name",
+            headerName: "Name",
+            flex: 0.5,
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+        },
+        {
+            field: "country",
+            headerName: "Country",
+            flex: 0.4,
+        },
+        {
+            field: "occupation",
+            headerName: "Occupation",
+            flex: 1,
+        },
+        {
+            field: "role",
+            headerName: "Role",
+            flex: 0.5,
+        },
+    ];
+
+    return (
+        <Box m="1.5rem 2.5rem">
+            <Header title="ADMINS" subtitle="List of available Admins" />
+            <Box
+                mt="40px"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderTop: "none",
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${theme.palette.secondary[200]} !important`,
+                    },
+                }}
+            >
+                <DataGrid
+                    loading={isLoading || !data}
+                    getRowId={(row) => row._id}
+                    rows={data || []}
+                    columns={columns}
+                />
+            </Box>
+        </Box>
+    )
+}
+
+export default Admin
